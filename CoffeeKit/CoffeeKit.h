f84c1161e68da9d13966e2c369dd0e834b59e35a
Rikard Olsson <rikard@thryselius.se>
2017-01-25 10:34:30 +0100

    Initial Commit

---
+++ CoffeeKit/CoffeeKit.h
@@ -0,0 +1,19 @@
+//
+//  CoffeeKit.h
+//  CoffeeKit
+//
+//  Created by Rikard Olsson on 2017-01-25.
+//  Copyright © 2017 Rikard Olsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoffeeKit.
+FOUNDATION_EXPORT double CoffeeKitVersionNumber;
+
+//! Project version string for CoffeeKit.
+FOUNDATION_EXPORT const unsigned char CoffeeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoffeeKit/PublicHeader.h>
+
+
