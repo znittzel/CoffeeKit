0afe7817fe363c48ec8fcf9a510131d441883fa3
Rikard Olsson <znittzel@gmail.com>
2017-02-08 23:23:03 +0100

    Added button and FAImage

---
+++ CoffeeKit/Button/CoffeeButton.swift
@@ -0,0 +1,35 @@
+//
+//  CoffeeButton.swift
+//  CoffeeKit
+//
+//  Created by Rikard Olsson on 2017-02-08.
+//  Copyright © 2017 Rikard Olsson. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+public class CoffeeButton: UIButton {
+
+    var is_init = false
+    
+    @IBInspectable public var cornerRadius: Double = 0 {
+        didSet {
+            self.layer.cornerRadius = CGFloat(self.cornerRadius)
+        }
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if !is_init {
+            is_init = true
+            
+            self.setupView()
+        }
+    }
+    
+    private func setupView() {
+        self.layer.cornerRadius = CGFloat(self.cornerRadius)
+    }
+}
---
+++ CoffeeKit/Image/FontAwesomeImage.swift
@@ -0,0 +1,56 @@
+//
+//  FontAwesomeImage.swift
+//  CoffeeKit
+//
+//  Created by Rikard Olsson on 2017-02-08.
+//  Copyright © 2017 Rikard Olsson. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+public class FontAwesomeImage: UIImageView {
+
+    var is_init = false
+    
+    @IBInspectable public var faColor: UIColor = UIColor.white {
+        didSet {
+            updateIcon()
+        }
+    }
+    
+    @IBInspectable public var faSize: Int = 12 {
+        didSet {
+            updateIcon()
+        }
+    }
+    
+    @IBInspectable public var faString: String? {
+        didSet {
+            self.updateIcon()
+        }
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if !is_init {
+            is_init = true
+            
+            // Update icon
+            self.updateIcon()
+        }
+    }
+    
+    private func updateIcon() {
+        guard let faString = self.faString else {
+            return
+        }
+        
+        guard let icon = UIImage.fontAwesomeIcon(code: faString, textColor: self.faColor, size: CGSize(width: self.faSize, height: self.faSize)) else {
+            return
+        }
+        
+        self.image = icon
+    }
+}
